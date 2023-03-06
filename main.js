ef8886d3bf09709b17b21501cc22f25b8923c660
suchet1212 <suchetreddy45@gmail.com>
2023-03-06 12:30:56 +0530

    modified:   main.js

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-myform=document.querySelector('my-form');
-name=document.querySelector('')
+myform=document.querySelector('#my-form');
+names=document.querySelector('#name');
+email=document.querySelector('#email');
+
+myform.addEventListener('submit',onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+
+    if(name.value===''||email.value===''){
+        alert('Enter your Username and Email ID');
+    }
+    else{
+        localStorage.setItem('name1',names.value);
+        localStorage.setItem('email1',email.value);
+    }
+}
