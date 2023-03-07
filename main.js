badc0134e763f826c44ee3b4923f743e283db380
suchet1212 <suchetreddy45@gmail.com>
2023-03-07 20:01:07 +0530

    modified:   main.js

--- main.js
+++ main.js
@@ -7,11 +7,20 @@ myform.addEventListener('submit',onSubmit);
 function onSubmit(e){
     e.preventDefault();
 
-    if(name.value===''||email.value===''){
+    if(names.value===''||email.value===''){
         alert('Enter your Username and Email ID');
     }
     else{
         localStorage.setItem('name1',names.value);
         localStorage.setItem('email1',email.value);
+
+        let myObj={
+            name:names.value,
+            email:email.value
+        };
+        myObj_serialized=JSON.stringify(myObj);
+        localStorage.setItem("MyObj",myObj_serialized);
+        let myObj_deserialized=JSON.parse(localStorage.getItem("MyObj"));
+        console.log(myObj_deserialized);
     }
 }
